d39b864d09b258b0a270c2a247bb1af422b98466
chen866 <1617383757@qq.com>
2025-01-06 00:26:16 +0800

    feat: script run_screen_save.py

---
+++ script/run_screen_save.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+import uiautomator2 as u2
+
+from phone_automation.common import package_dir
+
+d = u2.connect()
+# 获取 uiautomator2 的 Image 对象
+img_screen = d.screenshot()
+
+# 保存到图片文件夹
+img_dir = package_dir / "static" / "img" / f"screenshot_{datetime.now().strftime('%Y%m%d_%H%M%S')}.png"
+d.screenshot(img_dir)
